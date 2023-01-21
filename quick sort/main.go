7ec69f5419ab1549def4b53265d69a7f7234576d
Johnson Jan Su <su1221jan@gmail.com>
2023-01-22 00:03:45 +0800

    Add quick sort

---
+++ quick sort/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func partition(arr []int, low, high int) ([]int, int) {
+	pivot := arr[high]
+	i := low
+	for j := low; j < high; j++ {
+		if arr[j] < pivot {
+			arr[i], arr[j] = arr[j], arr[i]
+			i++
+		}
+	}
+	arr[i], arr[high] = arr[high], arr[i]
+	return arr, i
+}
+
+func quickSort(arr []int, low, high int) []int {
+	if low < high {
+		var p int
+		arr, p = partition(arr, low, high)
+		arr = quickSort(arr, low, p-1)
+		arr = quickSort(arr, p+1, high)
+	}
+	return arr
+}
+
+func generateSlice(size int) []int {
+	slice := make([]int, size)
+	rand.Seed(time.Now().UnixNano())
+	for i := 0; i < size; i++ {
+		slice[i] = rand.Intn(999) - rand.Intn(999)
+	}
+	return slice
+}
+
+func main() {
+	slice := generateSlice(30)
+	fmt.Println("\n--- Unsorted --- \n\n", slice)
+	slice = quickSort(slice, 0, len(slice)-1)
+	fmt.Println("\n--- Sorted ---\n\n", slice)
+}
