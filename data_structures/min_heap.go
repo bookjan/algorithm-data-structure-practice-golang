992fe30347777848984e46f8e9348021d5a6f838
Johnson Jan Su <su1221jan@gmail.com>
2023-02-25 23:18:18 +0800

    Update min_heap.go

--- data_structures/min_heap.go
+++ data_structures/min_heap.go
@@ -1,6 +1,11 @@
 package main
 
-import "container/heap"
+import (
+	"container/heap"
+	"fmt"
+)
+
+// This example used the built-in "container/heap package to implemented"
 
 type MinHeap []int
 
@@ -18,8 +23,10 @@ func (mh *MinHeap) Empty() bool {
 	return len(*mh) == 0
 }
 
+// If we want to have Max heap just change operator to GREATER THAN
+// such as: (*mh)[i] > (*mh)[j]
 func (mh *MinHeap) Less(i, j int) bool {
-	return (*mh)[i] < (*mh)[j]
+	return (*mh)[i] > (*mh)[j]
 }
 
 func (mh *MinHeap) Swap(i, j int) {
@@ -38,4 +45,14 @@ func (mh *MinHeap) Pop() interface{} {
 	v := (*mh)[len(*mh)-1]
 	*mh = (*mh)[0 : len(*mh)-1]
 	return v
+}
+
+func main() {
+	h := NewMinHeap()
+	list := []int{50, 60, 40, 30, 20, 10, 1}
+
+	for _, v := range list {
+		heap.Push(h, v)
+		fmt.Println(h)
+	}
 }
